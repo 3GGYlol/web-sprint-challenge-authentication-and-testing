8b37d5b271269e830fef4ce480795ec16edbbd63
3GGYlol <xlj135@gmail.com>
2024-11-05 11:02:50 -0500

    fin

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,18 +1,32 @@
-const jwt = require('jsonwebtoken');
+const {JWT_SECRET} = require('../secrets')
+const jwt = require('jsonwebtoken')
+
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization;
+  
+  /*
+    IMPLEMENT
 
-  if (!token) {
-    return res.status(401).json("token required");
-  }
+    1- On valid token in the Authorization header, call next.
 
-  jwt.verify(token, process.env.JWT_SECRET || "shh", (err, decoded) => {
-    if (err) {
-      return res.status(401).json("token invalid");
-    }
+    2- On missing token in the Authorization header,
+      the response body should include a string exactly as follows: "token required".
 
-    req.decodedToken = decoded;
-    next();
-  });
+    3- On invalid or expired token in the Authorization header,
+      the response body should include a string exactly as follows: "token invalid".
+
+  */
+      const token = req.headers.authorization
+      if(!token){
+       return res.status(401).json({message:'token required'})
+      }
+      jwt.verify(token,JWT_SECRET, (err,decodedToken)=>{
+        if(err){
+          res.status(401).json({message:'token invalid'})
+        }else{
+          req.decodedToken = decodedToken
+          next()
+        }
+      })
+
 };
